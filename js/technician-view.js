9e61c77e93366922049df8c172e2b799cdb3a6e1
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2022-12-05 19:10:47 +0000

    Added JavaScript to modal

---
+++ js/technician-view.js
@@ -0,0 +1,40 @@
+$(document).on("click", "#fault-card-1", function () {
+    var faultType = $("#fault-1-type").text();
+    var faultLocation = $("#fault-1-location").text();
+    var faultTime = $("#fault-1-time").text();
+    $("#modal-card-title").text(faultType);
+    $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-time").text(faultTime);
+});
+$(document).on("click", "#fault-card-2", function () {
+    var faultType = $("#fault-2-type").text();
+    var faultLocation = $("#fault-2-location").text();
+    var faultTime = $("#fault-2-time").text();
+    $("#modal-card-title").text(faultType);
+    $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-time").text(faultTime);
+});
+$(document).on("click", "#fault-card-3", function () {
+    var faultType = $("#fault-3-type").text();
+    var faultLocation = $("#fault-3-location").text();
+    var faultTime = $("#fault-3-time").text();
+    $("#modal-card-title").text(faultType);
+    $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-time").text(faultTime);
+});
+$(document).on("click", "#fault-card-4", function () {
+    var faultType = $("#fault-4-type").text();
+    var faultLocation = $("#fault-4-location").text();
+    var faultTime = $("#fault-4-time").text();
+    $("#modal-card-title").text(faultType);
+    $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-time").text(faultTime);
+});
+$(document).on("click", "#fault-card-5", function () {
+    var faultType = $("#fault-5-type").text();
+    var faultLocation = $("#fault-5-location").text();
+    var faultTime = $("#fault-5-time").text();
+    $("#modal-card-title").text(faultType);
+    $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-time").text(faultTime);
+});
