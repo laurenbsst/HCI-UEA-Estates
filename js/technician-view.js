b6f333d2b5ed1a7c2715a81373fb9f266da2d69b
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2022-12-06 17:47:28 +0000

    Fault descriptions added

--- js/technician-view.js
+++ js/technician-view.js
@@ -1,41 +1,56 @@
 $(document).on("click", "#fault-card-1", function () {
     var faultType = $("#fault-1-type").text();
     var faultLocation = $("#fault-1-location").text();
+    var faultInfo = $("#fault-1-additional-info").text();
     var faultTime = $("#fault-1-time").text();
     $("#modal-card-title").text(faultType);
     $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-additional-info").text(faultInfo);
     $("#modal-fault-time").text(faultTime);
 });
+
 $(document).on("click", "#fault-card-2", function () {
     var faultType = $("#fault-2-type").text();
     var faultLocation = $("#fault-2-location").text();
+    var faultInfo = $("#fault-2-additional-info").text();
     var faultTime = $("#fault-2-time").text();
     $("#modal-card-title").text(faultType);
     $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-additional-info").text(faultInfo);
     $("#modal-fault-time").text(faultTime);
 });
+
 $(document).on("click", "#fault-card-3", function () {
     var faultType = $("#fault-3-type").text();
     var faultLocation = $("#fault-3-location").text();
+    var faultInfo = $("#fault-3-additional-info").text();
     var faultTime = $("#fault-3-time").text();
     $("#modal-card-title").text(faultType);
     $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-additional-info").text(faultInfo);
     $("#modal-fault-time").text(faultTime);
 });
+
 $(document).on("click", "#fault-card-4", function () {
     var faultType = $("#fault-4-type").text();
     var faultLocation = $("#fault-4-location").text();
+    var faultInfo = $("#fault-4-additional-info").text();
     var faultTime = $("#fault-4-time").text();
     $("#modal-card-title").text(faultType);
     $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-additional-info").text(faultInfo);
     $("#modal-fault-time").text(faultTime);
 });
+
+
 $(document).on("click", "#fault-card-5", function () {
     var faultType = $("#fault-5-type").text();
     var faultLocation = $("#fault-5-location").text();
+    var faultInfo = $("#fault-5-additional-info").text();
     var faultTime = $("#fault-5-time").text();
     $("#modal-card-title").text(faultType);
     $("#modal-fault-location").text(faultLocation);
+    $("#modal-fault-additional-info").text(faultInfo);
     $("#modal-fault-time").text(faultTime);
 });
 
@@ -48,20 +63,28 @@ function hideMainIcon() {
     var selected_value = $('#status-dropdown').val();
 
     if(selected_value === null){
-        alert('You have not made any changes.');
+        alert('You have not made any changes!');
     }
     else if(selected_value === 'In-progress') {
         document.getElementById("fault-card-1").style.display = 'none';
 
-        var inProgressCard = document.createElement("button");
-        inProgressCard.className = 'fault-card';
-
-        inProgressCard.innerHTML = "Test";
-
         var card = document.getElementById("fault-card-1");
-      
+        var modal = document.getElementById("fault-card-modal");
+
         document.querySelector('.in-progress-jobs').appendChild(card);
         card.style.display = 'block';
         document.getElementById("main-icon").style.display = 'none';
+        $(modal).modal('hide');
+    }
+    else {
+        document.getElementById("fault-card-1").style.display = 'flex';
+
+        var card = document.getElementById("fault-card-1");
+        var modal = document.getElementById("fault-card-modal");
+      
+        document.querySelector('.completed-jobs').appendChild(card);
+        card.style.display = 'block';
+        document.getElementById("main-icon").style.display = 'none';
+        $(modal).modal('hide');
     }
 }
