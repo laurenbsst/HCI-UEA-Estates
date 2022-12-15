3ced58588119f3746dff45e1ac24027fea0802c6
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2022-12-15 12:33:26 +0000

    Added form validation

--- js/reporting-fault.js
+++ js/reporting-fault.js
@@ -1,3 +1,14 @@
 function validateForm(){
-    alert('hi!');
+    var typeOfFaultField = document.getElementById('type-of-fault');
+    var otherFaultTypeInput = document.getElementById('other-fault');
+
+    if(typeOfFaultField.value === 'Other'){
+        if(otherFaultTypeInput.value === ""){
+            alert('You selected the fault type "other", but did not enter more details. Please enter more details in the second field.');
+        }
+    }
+
+    alert('Fault report successfully submitted!');
+
+    return false;
 };
